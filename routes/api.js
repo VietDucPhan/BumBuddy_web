b78c479ed3a28bff0c489e9580a0fc2d0e3d58d8
viet_duc_phan <macbook@MACs-MacBook.local>
2016-08-04 13:20:40 +0700

    add routes get-bum

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,18 @@ var router = express.Router();
 var BumsModel = require('../models/BumsModel');
 
 /* GET users listing. */
+
 router.get('/get-bum', function(req, res, next) {
   BumsModel.get(function(err, respond){
-    res.json(respond);
+    return res.json(respond);
   })
 });
 
 router.post('/create-bum', function(req, res, next) {
   BumsModel.save(req.body, function(err, respond){
-    res.json(respond);
+    return res.json(respond);
   })
+
 });
 
 module.exports = router;

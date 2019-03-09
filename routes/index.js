514aaaa42260733ff30c356bc9489969e3164cf3
Viet Duc Phan <joomdaily@gmail.com>
2019-03-09 15:22:25 +0700

    add menu to frontpage

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var BumsModel = require('../models/BumsModel');
+var Config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   BumsModel.getAllBums(function(err, respond){
-    return res.render('index', { title: 'Bum Buddy'});
-  })
-
+    return res.render('index', { title: Config.sitename});
+  });
 });
 
 router.get('/privacy', function(req, res, next) {
-    return res.render('privacy', { title: 'Bum Buddy'});
+    return res.render('privacy', { title: Config.sitename});
 });
 
 router.get('/terms', function(req, res, next) {
-    return res.render('terms', { title: 'Bum Buddy'});
+    return res.render('terms', { title: Config.sitename});
 });
 
 router.get('/open-source-libraries', function(req, res, next) {
-    return res.render('openSourceLibraries', { title: 'Bum Buddy'});
+    return res.render('openSourceLibraries', { title: Config.sitename});
 });
 
 module.exports = router;
